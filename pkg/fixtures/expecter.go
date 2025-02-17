65886b704a8e7249b2408a9bcfe3021de8112571
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2025-02-17 19:26:47 +0600

    Update expecter.go
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- pkg/fixtures/expecter.go
+++ pkg/fixtures/expecter.go
@@ -11,3 +11,78 @@ type Expecter interface {
 type VariadicNoReturnInterface interface {
 	VariadicNoReturn(j int, is ...interface{})
 }
+
+type ExpecterNoArgArgs struct {
+	StrAnything bool
+}
+
+type ExpecterNoArgReturns struct {
+	Str string
+}
+
+type ExpecterNoArgExpectation struct {
+	Args    ExpecterNoArgArgs
+	Returns ExpecterNoArgReturns
+}
+
+type ExpecterNoReturnArgs struct {
+	Str         string
+	StrAnything bool
+}
+
+type ExpecterNoReturnReturns struct {
+}
+
+type ExpecterNoReturnExpectation struct {
+	Args    ExpecterNoReturnArgs
+	Returns ExpecterNoReturnReturns
+}
+
+type ExpecterManyArgsReturnsArgs struct {
+	Str         string
+	I           int
+	StrAnything bool
+	IAnything   bool
+}
+
+type ExpecterManyArgsReturnsReturns struct {
+	Strs []string
+	Err  error
+}
+
+type ExpecterManyArgsReturnsExpectation struct {
+	Args    ExpecterManyArgsReturnsArgs
+	Returns ExpecterManyArgsReturnsReturns
+}
+
+type ExpecterVariadicArgs struct {
+	Ints       []int
+	IntsAnything bool
+}
+
+type ExpecterVariadicReturns struct {
+	Err error
+}
+
+type ExpecterVariadicExpectation struct {
+	Args    ExpecterVariadicArgs
+	Returns ExpecterVariadicReturns
+}
+
+type ExpecterVariadicManyArgs struct {
+	I           int
+	A           string
+	Intfs       []interface{}
+	IAnything   bool
+	AAnything   bool
+	IntfsAnything bool
+}
+
+type ExpecterVariadicManyReturns struct {
+	Err error
+}
+
+type ExpecterVariadicManyExpectation struct {
+	Args    ExpecterVariadicManyArgs
+	Returns ExpecterVariadicManyReturns
+}
